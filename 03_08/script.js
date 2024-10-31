0fde1b6f71659ab6f0673e585de771b77311339e
Klarence <ouyang.klarence@gmail.com>
2024-10-30 22:44:31 -0500

    initial commit

--- 03_08/script.js
+++ 03_08/script.js
@@ -1,1 +1,19 @@
+const degreeInput = document.querySelector("#degrees");
+const box = document.querySelector(".box");
+// const valueToRotate = degreeInput.getAttribute("value");
+
+// degreeInput.addEventListener('change', rotateBox());
+degreeInput.addEventListener('change', () => {
+    // box.style.transform = `rotate(${degreeInput.value}deg)`;
+    // box.style.setProperty("transform", `rotate(${degreeInput.value}deg)`);
+    // box.style.transform = `rotate(${valueToRotate}deg)`
+});
+
+// function rotateBox() {
+//     box.style.transform = `rotate(${degreeInput.value}deg)`
+//     // box.style.transform = `rotate(${valueToRotate}deg)`
+//     console.log(valueToRotate);
+// }
+console.log(degreeInput);
+// console.log(valueToRotate);
 
